07caffcd8841f56f933b2088600a0c6cea083650
bamdad1381 <bamdadnikkhah@gmail.com>
2016-04-21 13:26:23 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,15 +3,14 @@ do
 local function run(msg, matches)
 local bot_id = 128555852 -- your bot id
    -- like local bot_id = 157738608
-    if matches[1] == 'leave' and is_sudo(msg) then
+    if matches[1] == 'sik' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
-    "^!!tgservice (.+)$",
+    "^sik$"
   },
   run = run
 }
